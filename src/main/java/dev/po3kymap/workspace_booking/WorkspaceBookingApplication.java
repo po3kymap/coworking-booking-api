19cba6034cf8b686f0e900c37b2c7f7a8886f77a
BITEP <87851407+PO3KYMAP@users.noreply.github.com>
2026-05-11 18:38:00 +0200

    Clean Spring Boot setup

---
+++ src/main/java/dev/po3kymap/workspace_booking/WorkspaceBookingApplication.java
@@ -0,0 +1,13 @@
+package dev.po3kymap.workspace_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/po3kymap/workspace_booking/WorkspaceBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.po3kymap.workspace_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
